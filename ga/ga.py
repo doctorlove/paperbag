a3cda79b4cc22d62cda01f87a1644a7df01ddeff
Frances Buontempo <frances.buontempo@gmail.com>
2013-06-05 11:57:16 +0100

    Start of projectiles (genetic algos) in python

---
+++ ga/ga.py
@@ -0,0 +1,30 @@
+
+def interpolate(a, b):
+    return a
+
+def collision_point(x0, y0, x1, y1, height, width):
+    x = 0.0
+    if x1 > width: x = width
+    m = (y1 - y0)/(1.0*x1 - x0)
+    y = y0 + m*(x-x0)
+    return y
+
+def collides(x0, y0, x1, y1, height, width):
+    if x0 >= 0 and x0 <= width and x1 >= 0 and x1 <= width:
+        return False
+    if (x0 <= 0 and x1 <= 0) or (x0 >= width and x1 >= width):
+        return False
+    y = collision_point(x0, y0, x1, y1, height, width)
+    return y < height
+
+
+if __name__ =="__main__":
+    height  =  5
+    width  =  10
+    x0 = width - 2
+    x1 = width + 2
+    y0 = height - 2
+    y1 = height + 4
+    print height + 1, collision_point(x0, y0, x1, y1, height, width)
+    print collides(x0, y0, x1, y1, height, width)
+
---
+++ ga/test_ga.py
@@ -0,0 +1,49 @@
+from ga import *
+import unittest
+
+class TestCollisions(unittest.TestCase):
+
+    def test_that_interpolation_of_horizontal_line_keeps_y_unchanged(self):
+        x, y  =  interpolate((0, 2), (1, 2))
+        self.assertEqual(2, y)
+
+    def test_that_ball_staying_inside_bag_does_not_collide(self):
+        height  =  5
+        width  =  10
+        x0 = 1
+        x1 = 0
+        y0 = 1
+        y1 = 1
+        self.assertFalse(collides(x0, y0, x1, y1, height, width))
+
+    def test_that_ball_staying_outside_bag_does_not_collide(self):
+        height  =  5
+        width  =  10
+        x0 = width + 1
+        x1 = width + 2
+        y0 = 0
+        y1 = 0
+        self.assertFalse(collides(x0, y0, x1, y1, height, width))
+
+    def test_that_ball_crossing_bag_does_collide(self):
+        height  =  5
+        width  =  10
+        x0 = width - 1
+        x1 = width + 2
+        y = height - 2
+        self.assertEqual(y, collision_point(x0, y, x1, y, height, width))
+        self.assertTrue(collides(x0, y, x1, y, height, width))
+
+    def test_that_ball_going_over_bag_does_not_collide(self):
+        height  =  5
+        width  =  10
+        x0 = width - 2
+        x1 = width + 2
+        y0 = height - 2
+        y1 = height + 4
+        self.assertEqual(height + 1, collision_point(x0, y0, x1, y1, height, width))
+        self.assertFalse(collides(x0, y0, x1, y1, height, width))
+
+
+if __name__  == '__main__':
+    unittest.main()
