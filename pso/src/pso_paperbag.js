826c70824f13df5c5ee29d873ea631219fe1889b
Fran Buontempo <FBuontempo@marexspectron.com>
2014-06-23 10:50:02 +0100

    Step towards moving th particles around for some epochs, without using gloabls

--- pso/src/pso_paperbag.js
+++ pso/src/pso_paperbag.js
@@ -1,11 +1,10 @@
-
-
 //do one particle first, majing sure you can see it trail around
 //See http://msdn.microsoft.com/en-us/magazine/hh335067.aspx
 //v(t+1) = (w * v(t)) + (c1 * r1 * (p(t) – x(t)) + (c2 * r2 * (g(t) – x(t))
 // x(t+1) = x(t) + v(t+1)
 
-function pso() {
+//are these by value?
+function draw(item, epoch) {
   var canvas = document.getElementById('tutorial');
   if (canvas.getContext) {
     var ctx = canvas.getContext("2d");
@@ -13,21 +12,48 @@ function pso() {
     ctx.fillStyle = "rgb(180, 120, 60)";
     ctx.fillRect (1, 8, canvas.width - 1, canvas.height - 8);
 
+    epoch = epoch + 1
     var result = document.getElementById("epoch");
-    result.innerHTML = parseInt(result.innerHTML) + 1;
+    result.innerHTML =  epoch;
 
-    ctx.fillStyle = "rgb(0,0,0)";
-    x = canvas.width/2.0;
-    y = canvas.height - canvas.height/2.0;//note the - to change y to canvas
-    ctx.fillRect (x, y - 2, 4, 4);
+    var i;
+    for (i = 0; i < item.length; ++i) {
+      ctx.fillStyle = "rgb(0,0,0)";
+      var particle = item[i];
+      ctx.fillRect (particle.x, particle.y - 2, 4, 4);
+    }
+  }
+  if (epoch < 5) {
+    setTimeout(function () { pso(item, epoch); }, 100);
   }
 }
+
+function pso(item, epoch) {
+  draw(item, epoch);
+}
+function initialise(particles){
+  var item = [];
+  var canvas = document.getElementById('tutorial');
+  var i;
+  for (i = 0; i < particles; ++i) {
+      x = canvas.width/2.0;
+      y = canvas.height - canvas.height/2.0;//note the - to change y to canvas
+      item.push ( { x: x, y: y } );
+  }
+  return item;
+}
+
+//draw
+//move, draw, move etc etc
+//See simulate function in aco
 
 function start() {
   var canvas = document.getElementById('tutorial');
   //...
   //
   document.getElementById("click_draw").innerHTML="stop"; 
-  id = setTimeout(pso, 100);
+  item = initialise(1);
+  var epoch = 1;
+  var id = setTimeout(function () { pso(item, epoch); }, 100);
 }
 
