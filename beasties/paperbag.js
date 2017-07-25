178d4deb521e6c5f2617ed220eb1c427b92c662e
Fran Buontempo <Frances.Buontempo@city.ac.uk>
2017-07-25 09:52:40 +0100

    Tidy up

--- beasties/paperbag.js
+++ beasties/paperbag.js
@@ -1,60 +1,63 @@
-var x = 150;
-var y = 150;
-var width = 4;
+function Particle(x, y) {
+  this.x = x;
+  this.y = y;
+}
 
 var id = 0;
 
 function init() {
-	if (id === 0) {
-		var milliseconds = new Date().getTime();
-		id = setInterval(function() {
-			update(milliseconds);
-			},
-			100); 
-	}
-	else {
-		clearInterval(id);
-		id = 0;
-		x = 150;
-		y = 150;
-	}
+  var c=document.getElementById("myCanvas");
+  var particle = new Particle(c.width/2, c.height/2);
+
+  if (id === 0) {
+    document.getElementById("Go").innerHTML="Stop";
+    id = setInterval(function() {
+      update(particle);
+      },
+      100); 
+  }
+  else {
+    clearInterval(id);
+    document.getElementById("Go").innerHTML="Start";
+    document.getElementById("demo").innerHTML="Success";
+    id = 0;
+  }
 }
 
-function in_bag(x_pos, y_pos, left, right, up, down) {
-	return (x_pos > left) && (x_pos < right) && (y_pos > up) && (y_pos < down);
+function in_bag(particle, left, right, up, down) {
+  return (particle.x > left) && (particle.x < right)
+	  && (particle.y > up) && (particle.y < down);
 }
 
-function update(milliseconds) {
-	//draw the bag
-	var c=document.getElementById("myCanvas");
-	var ctx=c.getContext("2d");
-	
-	ctx.clearRect(0,0,c.width,c.height); 
-	ctx.fillStyle="#E0B044";
-	var bag_width = 300;
-	var left = 75;
-	var right = left + bag_width;
-	var up = 25;
-	var down = up + bag_width;
-	ctx.fillRect(left,up,bag_width,bag_width);
-	
-        //Make this a move function...
-	x += bag_width * 0.2 * (-0.5 + Math.random());
-	y += bag_width * 0.2 * (-0.5 + Math.random());;
-	
-	ctx.beginPath();
-	ctx.rect(x,y,width,width);
-	ctx.strokeStyle="black";
-	ctx.stroke();
-	if (in_bag(x, y, left, right, up, down)) {
-		document.getElementById("Go").innerHTML="Stop";
-		document.getElementById("demo").innerHTML=new_x_move + ", " + new_y_move;
-	}
-	else {
-		document.getElementById("Go").innerHTML="Start";
-		document.getElementById("demo").innerHTML="Success";
-		var total_milliseconds = new Date().getTime() - milliseconds;
-		document.getElementById("time").innerHTML=total_milliseconds;
-		init();
-	}
+function draw(particle) {
+  var c=document.getElementById("myCanvas");
+  var ctx=c.getContext("2d");
+  
+  ctx.clearRect(0, 0, c.width, c.height);       //clear
+  ctx.fillStyle="#E0B044";
+  x = c.width/3;
+  y = c.height/3;
+  ctx.fillRect(x, y, c.width/3, c.height/3);    //draw bag
+
+  ctx.beginPath();
+  ctx.rect(particle.x, particle.y, 4, 4);
+  ctx.strokeStyle="black";
+  ctx.stroke();
+
+  return in_bag(particle, x, x+c.width/3, y, y+c.height/3);
 }
+
+
+function move(particle) {
+  particle.x += 50 * (Math.random() - 0.5);
+  particle.y += 50 * (Math.random() - 0.5);
+}
+
+function update(particle) {
+  move(particle);
+
+  if (!draw(particle)) {
+    init();
+  }
+}
+
